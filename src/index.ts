e524ae7aced32a90ca6dd55a78e056170ec40cb9
M.S.T.O.P <mstop4@gmail.com>
2024-11-24 21:30:07 -0500

    set up ejs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import path from 'path';
+import indexRouter from './routes/index';
+import authRouter from './routes/auth';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(morgan('combined'));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use('/', indexRouter);
+app.use('/', authRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ src/routes/auth.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+const router = Router();
+
+router.get('/login', (req, res) => res.render('login'));
+
+export default router;
